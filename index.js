82c88b53bff90d7c01c6bbce2284f2c327d5dd32
Lance-Mao <977986310@qq.com>
2018-05-06 17:41:59 +0800

    修复编译打包时不兼容es6语法，引入antd样式

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Forms from './src/form/Forms';
+
+ReactDOM.render(
+    <div>
+        <Forms/>
+    </div>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,48 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ProvidePlugin({
-      "$": "jquery",
-      "jQuery": "jquery",
-      "window.jQuery": "jquery"
-    })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        },
-        exclude: /node_modules/,
-        include: __dirname
-      }
-    ]
-  },
-  resolve: {
-    root: path.resolve(__dirname),
-    alias: {
-      'jquery-ui': 'jquery-ui-dist/jquery-ui'
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            "$": "jquery",
+            "jQuery": "jquery",
+            "window.jQuery": "jquery"
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'es2016', 'react'],
+                    plugins: ['babel-plugin-transform-class-properties',['import',{libraryName:'antd',style:'css'}]]
+                },
+                exclude: /node_modules/,
+                include: __dirname
+            },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+        ]
+    },
+    resolve: {
+        root: path.resolve(__dirname),
+        alias: {
+            'jquery-ui': 'jquery-ui-dist/jquery-ui'
+        }
     }
-  }
 }
